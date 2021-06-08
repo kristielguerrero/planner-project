7687a94d40e4998c63058a6dfd5ba110af208d1e
Kris Tina <kristyguerrero20@gmail.com>
2021-06-07 20:34:00 -0500

    added the columns and adjusted their size, added a save icon, added conditions to color the columns green, grey or red, and added the date in the header

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("inside the script file");
+
+moment().format("dddd, MMMM Do");
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+var timeBlockList = $(".time-block");
+console.log(timeBlockList);
+timeBlockList.each(function () {
+  console.log($(this).attr("id"));
+  var timeBlockHour = $(this).attr("id");
+  var currentTime = moment().hour();
+  if (timeBlockHour < currentTime) {
+    console.log("less than current time");
+    $(this).children("textarea").addClass("past");
+  } else if (timeBlockHour > currentTime) {
+    console.log("greater than current time");
+    $(this).children("textarea").addClass("present");
+  } else {
+    console.log("is equal");
+    $(this).children("textarea").addClass("future");
+  }
+});
+
+console.log(moment().hour());
